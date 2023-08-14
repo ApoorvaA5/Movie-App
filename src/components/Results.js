b2ca2608cb4f37d3df9ea601c2b1af886f5b0615
APOORVA  A <129650647+ApoorvaA5@users.noreply.github.com>
2023-08-14 08:35:03 +0530

    Add files via upload

---
+++ src/components/Results.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Result from "./Result";
+
+function Results({ results, openDetail }) {
+return (
+	<section className="results">
+	{typeof results != "undefined" ? (
+		results.map((result) => (
+		<Result key={result.imdbID} result
+			={result} openDetail={openDetail} />
+		))
+	) : (
+		<div className="not-found">
+		<h2>Sorry Movie not found </h2>
+		<h2>search for another movie.
+		</h2>
+		</div>
+	)}
+	</section>
+);
+}
+
+export default Results;
